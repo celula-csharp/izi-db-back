e75684a5b47ffe51dff8bd5e14e8d3985505e4a5
Farag0n <miguelanga1604@gmail.com>
2025-11-22 19:18:58 -0500

    fix: namespace

--- domain/Entities/User.cs
+++ domain/Entities/User.cs
@@ -1,19 +1,18 @@
-namespace IziDbBack.Domain.Entities
+namespace domain.Entities;
+
+public class User
 {
-    public class User
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        public string Username { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string PasswordHash { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PasswordHash { get; set; } = null!;
 
-        public bool IsActive { get; set; } = true;
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsActive { get; set; } = true;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
-        public int RoleId { get; set; }
-        public Role Role { get; set; } = null!;
+    public int RoleId { get; set; }
+    public Role Role { get; set; } = null!;
 
-        public UserInstance? UserInstance { get; set; }
-    }
+    public UserInstance? UserInstance { get; set; }
 }
