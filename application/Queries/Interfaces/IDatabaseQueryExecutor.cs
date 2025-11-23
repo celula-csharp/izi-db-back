78a7ae641f1694eabff1044c9a813dd9cd088d8d
Farag0n <miguelanga1604@gmail.com>
2025-11-22 20:09:42 -0500

    feat: new task

--- application/Queries/Interfaces/IDatabaseQueryExecutor.cs
+++ application/Queries/Interfaces/IDatabaseQueryExecutor.cs
@@ -1,9 +1,10 @@
-using application.Dtos;
+using application.Queries.Dtos;
 using domain.Enums;
 
-namespace application.Queries;
+namespace application.Queries.Interfaces;
 
 public interface IDatabaseQueryExecutor
 {
     Task<QueryResultDto> ExecuteQueryAsync(DatabaseType engine, string query, string connectionString);
+    Task<object> GetSchemaAsync(DatabaseType engine, string connectionString);
 }
